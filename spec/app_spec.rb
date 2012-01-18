1536387790764bab6f45cab54b656ed7d9bfca6a
Tomasz J <janecztom@gmail.com>
2012-01-18 22:22:42 +0100

    Test made to pass.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,10 +1,20 @@
 require 'app'
+require 'rspec'
 require 'rack/test'
 
+set :environment, :test
+
 describe "Sinatra test app" do
+  include Rack::Test::Methods
+  
+  def app
+    Sinatra::Application
+  end
+  
   describe "Routing" do
     it "should get the main page" do
-      get "/"
+      get '/'
+      last_response.should be_ok
     end
   end
 end

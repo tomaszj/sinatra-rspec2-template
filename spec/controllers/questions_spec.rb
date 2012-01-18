31d0d6a52017d8b86b823be804b919939707d1bc
Tomasz J <janecztom@gmail.com>
2012-01-19 00:04:23 +0100

    Added tests

---
+++ spec/controllers/questions_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+
+describe "Questions RESTful API" do
+  describe "Questions list" do
+    it "should load a list of questions" do
+      visit "/questions"
+      page.should have_content("Questions")
+    end
+  end
+  
+  describe "Show question" do
+    it "should display a question"
+      question = FactoryGirl.create(:question)
+      
+      visit "/questions/#{question.id}"
+      page.should have_content(question.question)
+  end
+  
+  describe "Add question" do
+    it "should add a valid question" do
+      @attr = {:question => "Question content"}
+      post "/questions", :question => @attr
+      Question.count.should == 1
+      page.should have_content(question.question)
+    end
+  end
+  
+  describe "Edit question" do
+    it "should change the question content" do
+      question = FactoryGirl.create(:question)
+      @attr = {:question => "Changed question"}
+      
+      put "/questions/#{question.id}", :question => @attr
+      question.reload!
+      question.question.should == @attr[:question]
+    end
+  end
+  
+  describe "Delete question" do
+    it "should delete the question" do
+      FactoryGirl.create(:question)
+      initial_count = Question.count
+      delete "/questions/#{question.id}"
+      Question.count.should == initial_count - 1
+      
+    end
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :question do
+    question "Question content"
+  end
+end

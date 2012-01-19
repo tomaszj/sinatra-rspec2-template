c6da86a7b07426fe29f9ef4557ccb03b6d42847c
Tomasz J <janecztom@gmail.com>
2012-01-19 01:30:23 +0100

    Added transactional functionality.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'rspec'
 require 'capybara/rspec'
 require 'rack/test'
 require 'factory_girl'
+require 'factories'
+require 'database_cleaner'
 
 set :environment, :test
 
@@ -15,4 +17,18 @@ Capybara.app = app
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
   conf.include Capybara::DSL
+  
+  # Database cleaner is needed to provide transaction functionality for Factory Girl
+  conf.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  conf.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  conf.after(:each) do
+    DatabaseCleaner.clean
+  end
 end

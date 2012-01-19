b47f9145c66b0d3a2472434e316593bd614a06d5
Tomasz J <janecztom@gmail.com>
2012-01-19 01:31:21 +0100

    Added Database access setup.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require 'sinatra'
+require 'data_mapper'
+
+configure do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/data/database.db")
+  
+  class Question
+    include DataMapper::Resource
+
+    validates_uniqueness_of :id
+
+    property :id, Serial
+    property :question, String
+  end
+  
+  DataMapper.auto_upgrade!
+end
+
+require 'controllers/questions_controller'
 
 get "/" do
   "Hello World!"

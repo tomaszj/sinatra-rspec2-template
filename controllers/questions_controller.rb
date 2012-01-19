35a571cb8772c32a38fa315c95b693796bf75e5e
Tomasz J <janecztom@gmail.com>
2012-01-20 00:11:07 +0100

    Added passing variables for rendering.

--- controllers/questions_controller.rb
+++ controllers/questions_controller.rb
@@ -1,12 +1,15 @@
 get "/questions" do
+  @questions = Question.all
   erb :"questions/index"
 end
 
 get "/questions/new" do
+  @question = Question.new
   erb :"questions/new"
 end
 
 get "/questions/:id" do |id|
+  @question = Question.get!(id)
   erb :"questions/show"
 end
 

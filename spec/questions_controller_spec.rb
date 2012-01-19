b20d4dcbd6f518f7f56759d914a62c31d751c9ea
Tomasz J <janecztom@gmail.com>
2012-01-19 19:31:00 +0100

    Added RESTful tests.

--- spec/questions_controller_spec.rb
+++ spec/questions_controller_spec.rb
@@ -2,53 +2,78 @@ require "spec_helper"
 
 describe "Questions RESTful API" do
   
-  before(:each) do
-    puts "Question count: #{Question.count}"
-  end
-  
   describe "Questions list" do
-    it "should load a list of questions" do
+    it "should be succesful" do
+      visit "/questions"
+    end
+    
+    it "should display a list of questions" do
       visit "/questions"
       page.should have_content("Questions")
     end
   end
   
   describe "Show question" do
+    before(:each) do
+      @question = FactoryGirl.create(:question)
+    end
+    
+    it "should be succesful" do
+      visit "/questions/#{@question.id}"
+    end
+    
     it "should display a question" do
-      question = FactoryGirl.create(:question)
-      
-      visit "/questions/#{question.id}"
-      page.should have_content(question.question)
+      visit "/questions/#{@question.id}"
+      page.should have_content(@question.question)
     end
   end
   
   describe "Add question" do
+    it "should be succesful" do
+      post "/questions"
+      last_response.should be_ok
+    end
+    
     it "should add a valid question" do
       @attr = FactoryGirl.build(:question).attributes
-      post "/questions", :question => @attr
-      Question.count.should == 1
+      expect {
+        post "/questions", :question => @attr
+      }.to change(Question, :count).by(1)
       page.should have_content(question.question)
     end
   end
   
   describe "Edit question" do
+    before(:each) do
+      @question = FactoryGirl.create(:question)
+    end
+    
+    it "should be succesful" do
+      visit "/questions/#{@question.id}/edit"
+    end
+    
     it "should change the question content" do
-      question = FactoryGirl.create(:question)
       @attr = {:question => "Changed question"}
-      
-      put "/questions/#{question.id}", :question => @attr
+      put "/questions/#{@question.id}", :question => @attr
       question.reload!
       question.question.should == @attr[:question]
     end
   end
   
   describe "Delete question" do
-    it "should delete the question" do
-      FactoryGirl.create(:question)
-      initial_count = Question.count
-      delete "/questions/#{question.id}"
-      Question.count.should == initial_count - 1
+    before(:each) do
+      @question = FactoryGirl.create(:question)
+    end
       
+    it "should be succesful" do
+      delete "/questions/#{@question.id}"
+      last_response.should be_ok
+    end
+    
+    it "should delete the question" do
+      expect {
+        delete "/questions/#{@question.id}"
+      }.to change(Question, :count).by(-1)
     end
   end
 end
